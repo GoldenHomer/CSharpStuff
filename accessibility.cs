461c369454fc97bf2d3a1a5c3701e1feb95a0df1
Jose Contreras <jocontre@joselcontreras.com>
2015-04-07 21:47:32 -0500

    Edit to file and creation of a new one

--- accessibility.cs
+++ accessibility.cs
@@ -5,7 +5,23 @@ using System.Text;
 
 namespace AccessSpecifier {
 	class Program {
-		protected int num; // private is default accesser.
+		// protected int num; // private is default accesser.
+
+		int num;
+		static int cnt;
+
+		public static void add() {
+			cnt++;
+		}
+		public void increment() {
+			num++;
+			cnt++;// num would have to be static as well to compile without error.
+		}
+
+		public void display() {
+			Console.WriteLine("num = {0} cnt = {1}", num, cnt);
+		}
+
 	}
 
 	class Test:Program {
@@ -14,6 +30,16 @@ namespace AccessSpecifier {
 			Test t = new Test();
 			p.num; // Cannot do this since num is protected. Limited to class Program.
 			t.num; // Valid since Test inherits from Program class.
+
+			Program.add();
 		}
 	}
 }
+
+/* 
+Two types of variables in a class: instance and static.
+static variables define constants, can be initialized outside member function or class definition
+and have only one copy of the variable for all the objects of a class.
+*/
+
+------
---
+++ static.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class FunctionCount {
+	public static int count = 0;
+
+	public static void CountFunction() {
+		count++;
+		System.Console.WriteLine("Count function is called by main {0} times.", count);
+	}
+
+	public static void Main(string[] args) {
+		for(int var = 0; var < 10; var++) {
+			FunctionCount.CountFunction();
+		}
+	}
+}
