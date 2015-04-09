b15f8d45fa4e4c436c04a86b804e3f8c61381259
Jose Contreras <jocontre@joselcontreras.com>
2015-04-08 20:32:32 -0500

    Constructors

---
+++ Construct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Construct // Two types of contructors, static and instance
+{
+    class Program
+    {
+        int num; // instance variable
+        static int cnt;
+
+        Program()
+        {
+            num = 100;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("{0}, p.num");
+
+            Program p2 = new Program();
+            Console.WriteLine("{0}", p2.num);
+        }
+    }
+}
