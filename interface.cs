bb6d65e19d6d68594b5842ecd8a7ad1749772afc
Jose Contreras <jocontre@joselcontreras.com>
2015-04-09 21:16:40 -0500

    meh

---
+++ interface.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace interface {
+	public interface I1 {
+		void display1();
+		void display2();
+	}
+
+	public interface I2 {
+		void display3();
+	}
+
+	class Program:I1{
+		static void Main(string[] args) {
+
+		}
+
+		#region I1 Members
+
+		public void display1(){
+
+		}
+
+		public void display2() {
+			
+		}
+	}
+}
