7b4e48c0c53e3fab6ea7f1f0ff70142a0ceac248
Jose Contreras <jocontre@joselcontreras.com>
2015-05-27 21:37:19 -0500

    back

---
+++ Inheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inheritance
+{
+    class Employee
+    {
+        protected int eid;
+        protected string name;
+    }
+
+    class Parttime:Employee
+    {
+        public int noh;
+        static void Main(string[] args)
+        {
+            Parttime p = new Parttime();
+            p.noh=5;
+            p.eid = 002;
+            p.name="Uncle Buck"
+        }
+    }
+
+    class fulltime:Employee
+    {
+        public int shift;
+    }
+}
